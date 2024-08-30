bdf6abaefd138775bd3946a5024194262100cc74
Vinay Anand Lodhi <vinayanandlodhi12@gmail.com>
2024-08-31 02:37:59 +0530

    map-functionality

--- Server/controllers/GetLocationController.js
+++ Server/controllers/GetLocationController.js
@@ -12,4 +12,35 @@ async function GetLocation(req, res) {
   }
 }
 
-module.exports = { GetLocation };
+async function AddLocation(req, res) {
+  try {
+    const { Latitude, Longitude, createdAt, updatedAt } = req.body;
+    const newLocation = new Location({
+      Latitude,
+      Longitude,
+      createdAt,
+      updatedAt,
+    });
+
+    await newLocation.save();
+    res.status(201).json(newLocation);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+}
+
+module.exports = { GetLocation , AddLocation };
+
+
+//async function GetLocation(req, res) {
+ // try {
+   // const locations = await Location.find().sort({ createdAt: -1 }); // Fetch all locations and sort by //creation date (newest first)
+  //  if (locations && locations.length > 0) {
+   //   res.status(200).json(locations); // Return all locations
+   // } else {
+  //    res.status(404).json({ message: "No locations found" });
+  //  }
+ //} catch (error) {
+ //   res.status(500).json({ message: "Server error", error });
+ // }
+//}
--- Server/routes/Locationroutes.js
+++ Server/routes/Locationroutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { GetLocation } = require("../controllers/GetLocationController");
+const { GetLocation , AddLocation } = require("../controllers/GetLocationController");
 const router = express.Router();
 
 router.get("/GetLocation", GetLocation);
+router.post("/AddLocation", AddLocation);
 
 module.exports = router;
